f1383b714cbb7e9613b3413a2f12146b5dd30426
jhonatan-pires <jhonatanpires636@gmail.com>
2020-10-20 21:51:55 -0300

    atualizando.

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,5 +1,65 @@
 package model;
 
-public class Aluno {
+import java.util.Date;
 
+abstract public class Aluno {
+
+    protected String Nome;
+    protected Float Matricula;
+    protected Date DataNasc;
+    protected double Np1;
+    protected double Np2;
+
+    public Aluno(String nome, Float matricula) {
+        Nome = nome;
+        Matricula = matricula;
+    }
+
+    public Aluno(String nome, Float matricula, Date dataNasc, double np1, double np2) {
+        Nome = nome;
+        Matricula = matricula;
+        DataNasc = dataNasc;
+        Np1 = np1;
+        Np2 = np2;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public Float getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(Float matricula) {
+        Matricula = matricula;
+    }
+
+    public Date getDataNasc() {
+        return DataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        DataNasc = dataNasc;
+    }
+
+    public double getNp1() {
+        return Np1;
+    }
+
+    public void setNp1(double np1) {
+        Np1 = np1;
+    }
+
+    public double getNp2() {
+        return Np2;
+    }
+
+    public void setNp2(double np2) {
+        Np2 = np2;
+    }
 }
--- src/model/dto/ProfessorDTO.java
+++ src/model/dto/ProfessorDTO.java
@@ -1,4 +1,4 @@
-package dto;
+package model.dto;
 
 public class ProfessorDTO {
 }
