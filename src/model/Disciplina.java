289f4abc62422c4ddbbca847747e1a0fb45cb945
Guga255 <guga255@hotmail.com>
2020-10-29 22:50:45 -0300

    Disciplina

--- src/model/Disciplina.java
+++ src/model/Disciplina.java
@@ -1,4 +1,18 @@
 package model;
 
 public class Disciplina {
+    public int CodDisc;
+    public String NomeDisc;
+    public float CargaHoraria;
+    public String AulasSemana;
 }
+
+public Disciplina (int codDisc, String nomeDisc, float cargaHoraria, String aulasSemana){
+    this.CodDisc = codDisc;
+    this.NomeDisc = nomeDisc;
+    this.CargaHoraria = cargaHoraria;
+    this.AulasSemana = aulasSemana;
+}
+
+
+
