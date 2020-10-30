717555cc2ac058339b37759e634d62c8376a84cb
Guga255 <guga255@hotmail.com>
2020-10-29 22:59:23 -0300

    Disciplina

--- src/model/Disciplina.java
+++ src/model/Disciplina.java
@@ -5,13 +5,40 @@ public class Disciplina {
     public String NomeDisc;
     public float CargaHoraria;
     public String AulasSemana;
-}
 
-public Disciplina (int codDisc, String nomeDisc, float cargaHoraria, String aulasSemana){
-    this.CodDisc = codDisc;
-    this.NomeDisc = nomeDisc;
-    this.CargaHoraria = cargaHoraria;
-    this.AulasSemana = aulasSemana;
+
+    public Disciplina (int codDisc, String nomeDisc, float cargaHoraria, String aulasSemana){
+        this.CodDisc = codDisc;
+        this.NomeDisc = nomeDisc;
+        this.CargaHoraria = cargaHoraria;
+        this.AulasSemana = aulasSemana;
+    }
+
+    public int getCodDisc(){
+        return CodDisc;
+    }
+    public void setCodDisc(int codDisc){
+        CodDisc = codDisc;
+    }
+    public String getNomeDisc(){
+        return NomeDisc;
+    }
+    public void setNomeDisc(String nomeDisc){
+        NomeDisc = nomeDisc;
+    }
+    public float getCargHoraria(){
+        return CargaHoraria;
+    }
+    public void setCargaHoraria(float cargaHoraria){
+        CargaHoraria = cargaHoraria;
+    }
+    public String getAulasSemana(){
+        return AulasSemana;
+    }
+    public void setAulasSemana(String aulasSemana){
+        AulasSemana = aulasSemana;
+    }
+
 }
 
 
